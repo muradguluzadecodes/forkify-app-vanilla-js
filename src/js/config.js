b0b22718bc0f9bdcc3b915ad160985814f6bd71a
muradguluzadecodes <quluzade.m.98@gmail.com>
2023-02-24 01:00:52 +0400

    project has successfully completed

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2a14beb5-9ef3-4b86-be00-19c3a9d378c9';
+export const MODAL_CLOSE_SEC = 2.5;
